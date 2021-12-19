f47c8badc05e20f490be815e6c448ce2d524f0eb
kauederp <36017642+kauederp@users.noreply.github.com>
2021-12-19 15:26:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p /opt/Tlauncher
 wget -c https://tlauncher.org/download/15326 -O /opt/Tlauncher/tlauncher.jar
 
 if test -f "/opt/Tlauncher/tlauncher.jar" ; then
-  echo "$(tput bold setaf 10)successfully installed.$(tput scrgr0)"
+  echo "$(tput bold setaf 10)successfully installed.$(tput sgr0)"
 else
   tput 1;
   echo "ERROR:$(tput sgr0) could not perform the installation. The link may be broken."
