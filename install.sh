851ef9ae9f3a56c9d707e1dc12a7ca9e55dcc043
kauederp <36017642+kauederp@users.noreply.github.com>
2021-12-19 15:21:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/env sh
 
-echo "$(whoami)"; [ "$UID" -eq 0  ] || exec sudo "$0" "$@"
+echo "$(whoami)"; [ "$UID" = "0" ] || exec sudo "$0" "$@"
 
 mkdir -p /opt/Tlauncher
 wget -c https://tlauncher.org/download/15326 -O /opt/Tlauncher/tlauncher.jar
 
-if [[ "/opt/Tlauncher/tlauncher.jar" -e ]];then
+if test -f "/opt/Tlauncher/tlauncher.jar" ; then
   echo "$(tput bold setaf 10)successfully installed.$(tput scrgr0)"
 else
   tput 1;
-  echo "ERROR:$(tput scrgr0) could not perform the installation. The link may be broken."
+  echo "ERROR:$(tput sgr0) could not perform the installation. The link may be broken."
 fi
-
